71593f108b1bb657fa444bcb73b2ae52d6995edd
Priyansh <pradadiya667@gmail.com>
2019-07-02 04:20:10 -0400

    removed padding and maid w-100 for row in about section to remove botoom scroll change img for hero section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -5,8 +5,8 @@ import priyansh from "./images/priyansh.jpg";
 function About() {
   return (
     <section id="about" className="about py-4 d-flex justify-content-center">
-      <div className="row bio-content py-5">
-        <div className="col-12 d-flex flex-column align-items-center">
+      <div className="row w-100 bio-content py-5">
+        <div className="col-12 p-0 d-flex flex-column align-items-center">
           <img src={priyansh} alt="Priyansh" />
           <p className="mt-5 mb-4 px-5">
             Hi, I'm Priyansh. I am a <span>full stack developer</span> having
