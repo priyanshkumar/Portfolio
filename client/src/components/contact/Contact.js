466827c65b07bd2a8b91b778356cb018874fe6ff
Priyansh <pradadiya667@gmail.com>
2019-08-03 22:25:43 -0400

    Email done with node app

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,85 +1,168 @@
 import React from "react";
+import ReactTimeout from "react-timeout";
 import "./Contact.css";
+import axios from "axios";
 
-function Contact() {
-  return (
-    <section className="row contact m-0 py-5 contact-background">
-      <div className="container py-lg-5">
-        <div className="row">
-          <div className="col-lg-5 col-sm-12 d-flex flex-column justify-content-center align-items-center">
-            <div className="row">
-              <div className="col-12">
-                <h2 className="text-center">
-                  <span className="heading-border"> Get in touch</span>
-                </h2>
+class Contact extends React.Component {
+  state = {
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+    error: "",
+    sent: ""
+  };
+
+  handleChange = event => {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+      error: ""
+    });
+  };
+
+  handleSubmit = () => {
+    axios
+      .post("/api/contact", this.state)
+      .then(response => {
+        if (response.data.error) {
+          this.setState({
+            error: response.data.error
+          });
+        }
+        if (response.data.sent) {
+          this.setState({
+            sent: response.data.sent,
+            email: "",
+            name: "",
+            message: "",
+            subject: ""
+          });
+          this.props.setTimeout(() => {
+            this.setState({
+              sent: ""
+            });
+          }, 15000);
+        }
+      })
+      .then(() => {
+        this.setState({
+          email: "",
+          name: "",
+          message: "",
+          subject: ""
+        });
+      });
+  };
+  render() {
+    return (
+      <section className="row contact m-0 py-5 contact-background">
+        <div className="container py-lg-5">
+          <div className="row">
+            <div className="col-lg-5 col-sm-12 d-flex flex-column justify-content-center align-items-center">
+              <div className="row">
+                <div className="col-12">
+                  <h2 className="text-center">
+                    <span className="heading-border"> Get in touch</span>
+                  </h2>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-12 d-flex justify-content-center align-items-center mt-4 my-lg-4">
+                  <ul className="contact-details text-center p-0">
+                    <li>28 Fandor Way</li>
+                    <li>Brampton, Canada</li>
+                    <li>+1 (705) 7337875</li>
+                    <li>pradadiya667@gmail.com</li>
+                  </ul>
+                </div>
               </div>
             </div>
-            <div className="row">
-              <div className="col-12 d-flex justify-content-center align-items-center mt-4 my-lg-4">
-                <ul className="contact-details text-center p-0">
-                  <li>28 Fandor Way</li>
-                  <li>Brampton, Canada</li>
-                  <li>+1 (705) 7337875</li>
-                  <li>pradadiya667@gmail.com</li>
-                </ul>
-              </div>
+            <div className="col-lg-7 col-12">
+              <form className="contact-form">
+                {this.state.error && (
+                  <div className="d-flex">
+                    <div className="py-1 m-0 alert alert-danger">
+                      {this.state.error}
+                    </div>
+                  </div>
+                )}
+                {this.state.sent && (
+                  <div className="d-flex">
+                    <div className="py-1 m-0 alert alert-success">
+                      {this.state.sent}
+                    </div>
+                  </div>
+                )}
+                <div className="form-row my-3">
+                  <div className="col-lg-6 col-12">
+                    <input
+                      type="text"
+                      className="form-control py-2 mb-3 mb-lg-0"
+                      placeholder="Your Name"
+                      name="name"
+                      value={this.state.name}
+                      onChange={this.handleChange}
+                      required
+                    />
+                  </div>
+                  <div className="col-lg-6 col-12">
+                    <input
+                      type="Email"
+                      className="form-control py-2"
+                      placeholder="Email"
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.handleChange}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="form-row my-3">
+                  <div className="col-12">
+                    <input
+                      type="text"
+                      className="form-control py-2"
+                      placeholder="Subject"
+                      name="subject"
+                      value={this.state.subject}
+                      onChange={this.handleChange}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="form-row my-3">
+                  <div className="col-12">
+                    <textarea
+                      className="form-control py-2"
+                      aria-label="With textarea"
+                      placeholder="Message"
+                      name="message"
+                      value={this.state.message}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="form-row mt-3">
+                  <div className="col-12">
+                    <button
+                      type="button"
+                      className="btn btn-secondary btn-block py-2"
+                      onClick={() => {
+                        this.handleSubmit();
+                      }}
+                    >
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
-          </div>
-          <div className="col-lg-7 col-12">
-            <form className="contact-form">
-              <div className="form-row my-3">
-                <div className="col-lg-6 col-12">
-                  <input
-                    type="text"
-                    className="form-control py-2 mb-3 mb-lg-0"
-                    placeholder="Your Name"
-                    required
-                  />
-                </div>
-                <div className="col-lg-6 col-12">
-                  <input
-                    type="Email"
-                    className="form-control py-2"
-                    placeholder="Email"
-                    required
-                  />
-                </div>
-              </div>
-              <div className="form-row my-3">
-                <div className="col-12">
-                  <input
-                    type="text"
-                    className="form-control py-2"
-                    placeholder="Subject"
-                    required
-                  />
-                </div>
-              </div>
-              <div className="form-row my-3">
-                <div className="col-12">
-                  <textarea
-                    className="form-control py-2"
-                    aria-label="With textarea"
-                    placeholder="Message"
-                  />
-                </div>
-              </div>
-              <div className="form-row mt-3">
-                <div className="col-12">
-                  <button
-                    type="button"
-                    className="btn btn-secondary btn-block py-2"
-                  >
-                    Submit
-                  </button>
-                </div>
-              </div>
-            </form>
           </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
-
-export default Contact;
+export default ReactTimeout(Contact);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const nodemaile = require("nodemailer");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const app = express();
 
@@ -11,6 +12,72 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.post("/api/contact", (req, res) => {
+  if (!req.body.name) {
+    res.json({ error: "Please provide your fullname!" });
+  } else if (!req.body.email) {
+    res.json({ error: "Please provide your email" });
+  } else if (!req.body.subject) {
+    res.json({ error: "Please provide the subject for contact!" });
+  } else if (!req.body.message) {
+    res.json({ error: "Please provide the description!" });
+  } else {
+    let transporter = nodemailer.createTransport({
+      service: "gmail", // true for 465, false for other ports
+      auth: {
+        user: process.env.user, // generated ethereal user
+        pass: process.env.pass // generated ethereal password
+      }
+    });
+
+    // send mail with defined transport object
+    transporter.sendMail(
+      {
+        from: "pradadiya667@gmail.com", // sender address
+        to: req.body.email, // list of receivers
+        subject: "Conformation!", // Subject line
+        html: `<h4>Hello ${
+          req.body.name
+        },</h4><p>This is to verify that Priyansh Radadiya has recieved your email.</p><p>He will be contacting you within 2 business day.</p><p>For any urgent inquires you can contact him on 7057337875</p><p>Thankyou</p><p>Priyansh</p>` // plain text body
+      },
+      (err, data) => {
+        if (err) {
+          console.log(err);
+          res.json({
+            error:
+              "There might be something wrong. Please check the email or try again later."
+          });
+        } else {
+          console.log("email sent");
+          res.json({
+            sent:
+              "Hey you must have recived an email. If not check your email and submit again."
+          });
+        }
+      }
+    );
+    transporter.sendMail(
+      {
+        from: "pradadaiya667@gmail.com", // sender address
+        to: "pradadiya667@gmail.com", // list of receivers
+        subject: "From PR Portfolio", // Subject line
+        html: `<h2>Hi Priyansh you got a new message</h2>
+        <p>name: ${req.body.name}</p>
+        <p>email: ${req.body.email}</p><p>subject: ${
+          req.body.subject
+        }</p><p>message: ${req.body.message}</p><p>ThankYou</p>` // plain text body
+      },
+      (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("email sent");
+        }
+      }
+    );
+  }
+});
 
 app.listen(PORT, () => {
   console.log("App listening on PORT " + PORT);
